518498978fa6f9565bde2b250154dbfe964ac501
timothymmurphy <41353945+timothymmurphy@users.noreply.github.com>
2021-02-02 15:18:05 -0700

    Added Get-MimecastAccount

---
+++ MimecastPS/Public/Get-MimecastAccount.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+    Retrieves Mimecast account information.
+    Official Mimecast documentation: https://www.mimecast.com/tech-connect/documentation/endpoint-reference/account/get-account/
+
+    .EXAMPLE
+    Get-MimecastAccount
+
+#>
+
+function Get-MimecastAccount {
+    param ()
+
+    $jsonBody =  "{
+        ""data"": [
+        ]
+    }"
+
+    $Parameters = @{
+        Uri           = "/api/account/get-account"
+        Method        = "Post"
+        Body          = $jsonBody
+    }
+
+    $result = Invoke-MimecastMethod @Parameters
+
+    $result
+}
